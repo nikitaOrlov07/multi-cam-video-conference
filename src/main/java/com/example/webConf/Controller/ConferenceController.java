5b6f374c3850cc96744d8477ac1b2cabb65caa3c
Nikita Orlov <nikitaorlov145@gmail.com>
2024-10-26 14:30:58 +0200

    Add session security configuration , initial page , fixed device-setting page: add microphone sound check

--- src/main/java/com/example/webConf/Controller/ConferenceController.java
+++ src/main/java/com/example/webConf/Controller/ConferenceController.java
@@ -2,22 +2,56 @@ package com.example.webConf.Controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @Slf4j
 public class ConferenceController {
-
-
-    @GetMapping("/api/room")
-    @ResponseBody
-    public String getRoomName() {
-        String randomRoomName = UUID.randomUUID().toString();
-        log.info("Generated room name: {}", randomRoomName);
-        return  randomRoomName;
+    @GetMapping("/home")
+    public String getHomePage()
+    {
+        log.info("Home page is working");
+        return  "initial-page";
     }
+    @GetMapping("/setDevices")
+    public String getAvailableCameras(@RequestParam(value = "userName", required = false) String userName,
+                                      Model model)
+    {
+        log.info("Initial device setting page is working");
+        model.addAttribute("userName");
+        return "device-setting";
+    }
+    // Метод для приема POST-запроса с выбранными камерами
+    @PostMapping("/connect-devices")
+    public String connectDevices(@RequestBody Map<String, List<String>> requestBody, RedirectAttributes redirectAttributes) {
+        log.info("\"connectDevices\" controller method is working");
+        List<String> selectedCameras = requestBody.get("cameras");
+        List<String> selectedAudio = requestBody.get("audio");
+
+        boolean noCamerasSelected = selectedCameras == null || selectedCameras.isEmpty();
+        boolean noMicrophonesSelected = selectedAudio == null || selectedAudio.isEmpty();
+
+        if (noCamerasSelected) {
+            redirectAttributes.addAttribute("noCamerasSelected", true);
+        }
+
+        if (noMicrophonesSelected) {
+            redirectAttributes.addAttribute("noMicrophoneSelected", true);
+        }
+
+        if (!noCamerasSelected || !noMicrophonesSelected) {
+            // Логика обработки выбранных устройств
+            log.info("Selected cameras: {}", String.join(", ", selectedCameras));
+            log.info("Selected audio devices: {}", String.join(", ", selectedAudio));
+        }
+
+        return "redirect:/setDevices";
+    }
+
+
 }
